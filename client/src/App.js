3b1223729e1adf98b3f828e9c23ba0c0f55c0093
Jinyoung Park <cruiff79@gmail.com>
2020-04-11 20:48:30 -0600

    Update UI with grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,78 @@
 import React from 'react';
 import './App.css';
 import Movie from './components/Movie';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, withStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  apps: {
+    backgroundColor: 'black'
+  },
+  control: {
+    padding: 50,
+    paddingLeft: '20%',
+    paddingRight: '20%',
+  },  
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+});
 
 class App extends React.Component {
   constructor(props) {
@@ -24,19 +96,56 @@ class App extends React.Component {
   }
 
   render() {
+    const { classes } = this.props;
+
     return (
-      <div className="App">
-        {this.state.movies ? 
-          this.state.movies.map(item => {
-            return (
-              <Movie key={item.imdbID} title={item.Title} poster={item.Poster} year={item.Year} />
-            );
-          })
-          : ''
-        }
+      <div className={classes.root}>
+        <AppBar position="static" className={classes.apps}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="open drawer"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" noWrap>
+              Movie World
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+
+        <Grid container className={classes.control}>
+            <Grid item xs={12}>
+              <Grid container justify="center">
+              {this.state.movies ? 
+                this.state.movies.map(item => {
+                  return (
+                    <Movie key={item.imdbID} title={item.Title} poster={item.Poster} year={item.Year} />
+                  );
+                })
+                : ''
+              }
+              </Grid>
+            </Grid>
+        </Grid>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-class Movie extends React.Component {
+class Movie extends React.Component {      
     render() {
+        const { classes } = this.props;
         return (
-            <div>
-                <a href=""><img src={this.props.poster} alt="Poster" style={{width:128, height:192}}/></a>
-                <h4>{this.props.title}</h4>
+            <div className="grid_poster">
+                <a href=""><img src={this.props.poster} alt="Poster" className="poster" /></a>
+                <strong>{this.props.title}</strong><br/>
                 {this.props.year}
             </div>
         );
