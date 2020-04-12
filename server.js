ebfa81f1684e5f479c979ebfafc0ffd0a2288086
Jinyoung Park <cruiff79@gmail.com>
2020-04-11 21:23:27 -0600

    configure to connect with mysql DB

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 5000;
 
 const movies = require(__dirname + '/movie.json');
+
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
 
 app.get('/movie', (req, res) => {
     console.log(movies);
