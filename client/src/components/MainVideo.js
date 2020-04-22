5f406d24a8a3078fd0d010f06c8a0639c70191f8
Jinyoung Park <cruiff79@gmail.com>
2020-04-21 21:24:12 -0600

    update main page to list popular and top rated

---
+++ client/src/components/MainVideo.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import VideoList from './VideoList';
+import {Row, Col, Nav} from 'react-bootstrap';
+
+class MainVideo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          videos: '',
+          type: 'movie'
+        };
+    }
+
+    componentDidMount() {
+        this.handleVideo(this.state.type);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if(this.state.type !== nextState.type) {
+            this.handleVideo(nextState.type);
+        }
+        return true;
+    }
+
+    handleVideo = (type) => {
+        this.callVideos(type)
+          .then(res => this.setState({videos: res}))
+          .catch(err => console.log(err));
+    }
+
+    callVideos = async (type) => {
+        let url = `/api/video/${this.props.title}/${type}`;
+        const response = await fetch(url);
+        const body = await response.json();
+        return body;
+    }
+
+    getVideoInfo = (id, title) => {
+        this.props.getVideoInfo(id, title);
+    }
+
+    render() {
+        let subTitle = 'Popular';
+        if(this.props.title === 'topRated') subTitle = 'Top Rated';
+        return (
+            <div>
+                <Nav className="subTitle" variant="pills" defaultActiveKey="#movie">
+                    <Nav.Item className="navItem">
+                        <h3>{subTitle}</h3>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#movie" onClick={() => {this.setState({type: 'movie'});}}>Movie</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#tv" onClick={() => {this.setState({type: 'tv'});}}>TV</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+                <Row className="show-grid movie-list">
+                    {this.state.videos ?
+                        this.state.videos.map(item => {
+                            return (
+                                <Col>
+                                    <VideoList
+                                        types={item.type}
+                                        getVideoInfo={this.getVideoInfo}
+                                        key={item.id}
+                                        id={item.id}
+                                        title={item.title}
+                                        poster={item.poster}
+                                        year={item.year}
+                                    />
+                                </Col>
+                            );
+                        })
+                    : ''
+                    }
+                </Row>
+            </div>
+        );
+    }
+}
+
+export default MainVideo;
--- client/src/container/Main.js
+++ client/src/container/Main.js
@@ -1,43 +1,11 @@
 import React from 'react';
-import VideoList from '../components/VideoList';
-import {Container, Row, Col, Carousel, Nav} from 'react-bootstrap';
+import MainVideo from '../components/MainVideo';
+import {Container, Carousel} from 'react-bootstrap';
 import mainImage1 from '../images/main-image1.jpg';
 import mainImage2 from '../images/main-image2.jpg';
 import mainImage3 from '../images/main-image3.jpg';
 
 class Main extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          popular: '',
-          topRated: ''
-        };
-    }
-
-    componentDidMount() {
-        this.callPopular()
-          .then(res => this.setState({popular: res}))
-          .catch(err => console.log(err));
-
-        this.callTopRated()
-          .then(res => this.setState({topRated: res}))
-          .catch(err => console.log(err));
-    }
-
-    callPopular = async () => {
-        // const response = await fetch('/api/video/popular/' + this.props.types);
-        const response = await fetch('/api/video/popular/movie');
-        const body = await response.json();
-        return body;
-    }
-
-    callTopRated = async () => {
-        // const response = await fetch('/api/video/topRated/' + this.props.types);
-        const response = await fetch('/api/video/topRated/movie');
-        const body = await response.json();
-        return body;
-    }
-
     getVideoInfo = (id, title) => {
         this.props.handleVideoInfo(id, title);
     }
@@ -57,69 +25,9 @@ class Main extends React.Component {
                             <img className="d-block w-100" src={mainImage3} alt="Third slide" />
                         </Carousel.Item>
                     </Carousel>
-                    <Nav className="subTitle" variant="pills" defaultActiveKey="#first">
-                        <Nav.Item className="navItem">
-                            <h3>Popular</h3>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="#first">Movie</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="#link">TV</Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                    <Row className="show-grid movie-list">
-                        {this.state.popular ?
-                            this.state.popular.map(item => {
-                                return (
-                                    <Col>
-                                        <VideoList
-                                            types={item.type}
-                                            getVideoInfo={this.getVideoInfo}
-                                            key={item.id}
-                                            id={item.id}
-                                            title={item.title}
-                                            poster={item.poster}
-                                            year={item.year}
-                                        />
-                                    </Col>
-                                );
-                            })
-                        : ''
-                        }
-                    </Row>
-                    <Nav className="subTitle" variant="pills" defaultActiveKey="#first">
-                        <Nav.Item className="navItem">
-                            <h3>Top Rated</h3>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="#first">Movie</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="#link">TV</Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                    <Row className="show-grid movie-list">
-                        {this.state.topRated ?
-                            this.state.topRated.map(item => {
-                                return (
-                                    <Col>
-                                        <VideoList
-                                            types={item.type}
-                                            getVideoInfo={this.getVideoInfo}
-                                            key={item.id}
-                                            id={item.id}
-                                            title={item.title}
-                                            poster={item.poster}
-                                            year={item.year}
-                                        />
-                                    </Col>
-                                );
-                            })
-                        : ''
-                        }
-                    </Row>
-                </Container>                
+                    <MainVideo title='popular' getVideoInfo={this.getVideoInfo} />
+                    <MainVideo title='topRated' getVideoInfo={this.getVideoInfo} />
+                </Container>
             </div>
         );
     }
